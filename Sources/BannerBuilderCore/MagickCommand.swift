b307dfd1a6d38e87f1fff844c65ba634ce1aab0c
Isaac Halvorson <hello@hisaac.net>
2018-11-20 12:12:51 -0600

    Attempt to get process script to run

--- Sources/BannerBuilderCore/MagickCommand.swift
+++ Sources/BannerBuilderCore/MagickCommand.swift
@@ -1,6 +1,7 @@
 // Created by Isaac Halvorson on 11/14/18
 
 import Basic
+import Foundation
 
 struct MagickCommand {
 
@@ -46,9 +47,9 @@ struct MagickCommand {
 		do {
 			let inputFiles = try localFileSystem.getDirectoryContents(inputPath)
 			self.inputImages = inputFiles.compactMap {
-				let path = AbsolutePath($0)
-				if path.extension == "png" {
-					return path
+				let relativePath = RelativePath($0)
+				if relativePath.extension == "png" {
+					return AbsolutePath(inputPath, relativePath)
 				} else {
 					return nil
 				}
@@ -68,8 +69,10 @@ extension MagickCommand {
 
 			let commandString = buildMagickCommand(for: image)
 
+			#warning("Figure out how to launch these processes")
+
 			do {
-				let process = Process(args: commandString)
+				let process = Process(arguments: ["-c", buildMagickCommand(for: image)])
 				try process.launch()
 				let result = try process.waitUntilExit()
 				let output = try result.utf8Output()
@@ -86,31 +89,30 @@ extension MagickCommand {
 
 			return """
 				magick \
-					\(image.asString) \
-					-set filename:input "%t" \
-					-fill "rgb(\(bannerColor.rgbString))" \
-					-draw "polygon 0,%[fx:h/3] 0,%[fx:h-h/3] %[fx:w/3],%h %[fx:w-w/3],%h" \
-					-fill "rgb(\(textColor.rgbString)" \
-					-gravity \(gravity) \
-					-pointsize "%[fx:h/5]" \
-					-draw "rotate 45 font AndaleMono text %[fx:h/4],%[fx:h/\(horizontalPosition)] '\(bannerText)'" \
-					\(outputPath.asString)%[filename:input].png
+				\(image.asString) \
+				-set filename:input "%t" \
+				-fill "rgb(\(bannerColor.rgbString))" \
+				-draw "polygon 0,%[fx:h/3] 0,%[fx:h-h/3] %[fx:w/3],%h %[fx:w-w/3],%h" \
+				-fill "rgb(\(textColor.rgbString)" \
+				-gravity \(gravity) \
+				-pointsize "%[fx:h/5]" \
+				-draw "rotate 45 font AndaleMono text %[fx:h/4],%[fx:h/\(horizontalPosition)] '\(bannerText)'" \
+				\(outputPath.asString)%[filename:input].png
 				"""
 
 		} else {
 
 			return """
 				magick \
-					\(image.asString) \
-					-set filename:input "%t" \
-					-gravity \(gravity) \
-					-fill "rgb(\(textColor.rgbString))" \
-					-undercolor "rgb(\(bannerColor.rgbString))" \
-					-pointsize "%[fx:h/5]" \
-					-annotate +0+0 "\\        \(bannerText)        " \
-					\(outputPath.asString)%[filename:input].png
-			"""
-
+				\(image.asString) \
+				-set filename:input "%t" \
+				-gravity \(gravity) \
+				-fill "rgb(\(textColor.rgbString))" \
+				-undercolor "rgb(\(bannerColor.rgbString))" \
+				-pointsize "%[fx:h/5]" \
+				-annotate +0+0 "\\        \(bannerText)        " \
+				\(outputPath.asString)%[filename:input].png
+				"""
 		}
 	}
 
